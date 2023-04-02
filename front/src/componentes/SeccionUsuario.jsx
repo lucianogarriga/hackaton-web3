58594bf557198ece06114c34d31ae65377a86d16
JuaniSO <jibocchi14@gmail.com>
2023-04-02 15:53:46 -0300

    second merge - roles -

--- front/src/componentes/SeccionUsuario.jsx
+++ front/src/componentes/SeccionUsuario.jsx
@@ -1,10 +1,58 @@
+import { useEffect, useState } from "react";
+import { useAccount } from "wagmi";
 import "./SeccionUsuario.css";
-import UserInfo from "./UserInfo";
+const users = require("../users/users.json");
 
-const SeccionUsuario = () => {
+const UserInexistente = () => {
   return (
     <section className="seccion-usuario">
-      <UserInfo />
+      <span>Usuario Inexistente</span>
+    </section>
+  );
+};
+
+const UserInquilino = ({ user }) => {
+  const pagarExpensa = () => {
+    console.log("pagar expensa");
+  };
+
+  return (
+    <section className="seccion-usuario">
+      <span className="user">Welcome {user._name}</span>
+      <span className="user">Monto: {user._amount}</span>
+      <button onClick={pagarExpensa}>Pay Expenses</button>
+    </section>
+  );
+};
+
+const UserAdmin = ({ user }) => {
+  const cobrarSueldo = () => {
+    console.log("Cobrar Sueldo");
+  };
+
+  return (
+    <section className="seccion-usuario">
+      <span className="user">Welcome Admin {user._name}</span>
+      <span className="user">Balance: {user._amount}</span>
+      <button onClick={cobrarSueldo}>Add Tenant</button>
+      <button onClick={cobrarSueldo}>Add Employeed</button>
+      <button onClick={cobrarSueldo}>Pay Salary</button>
+    </section>
+  );
+};
+
+const SeccionUsuario = () => {
+  const { address, isConnecting, isDisconnected } = useAccount();
+
+  let user = users.find((user) => {
+    return user._userAddress === address;
+  });
+
+  return (
+    <section>
+      {!user && <UserInexistente />}
+      {user && user._role === "inquilino" && <UserInquilino user={user} />}
+      {user && user._role === "admin" && <UserAdmin user={user} />}
     </section>
   );
 };
--- front/src/paginas/PaginaHome.jsx
+++ front/src/paginas/PaginaHome.jsx
@@ -1,24 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { Header } from "../componentes/Header";
-
 import "./PaginaHome.css";
 import { SeccionUsuario } from "../componentes/SeccionUsuario";
 import ConnectUser from "../componentes/ConnectUser";
+import Loader from "../componentes/Loader";
 
 // import { useContractReads } from 'wagmi'
-// import { useContract } from 'wagmi'
 
 const PaginaHome = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [addressUser, setAddressUser] = useState("");
   const [estaConectado, setEstaConectado] = useState(false);
-
+  // const {address} = getAccount()
+  if (isLoading) <Loader />;
   return (
     <section className="bodyBorder">
       <Header />
       <ConnectUser
         setEstaConectado={setEstaConectado}
         estaConectado={estaConectado}
+        setAddressUser={setAddressUser}
+        setIsLoading={setIsLoading}
       />
-      {estaConectado && <SeccionUsuario />}
       {/* <div>{balance !== undefined ?? <p>BALANCE: {balance}</p>}</div> */}
     </section>
   );
